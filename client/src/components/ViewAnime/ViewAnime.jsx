5d82bc15bc37f8ef94efbcafd47e1b1ffd481280
gino <gamexgaming1997@gmail.com>
2022-08-19 23:18:36 +0800

    commit

--- client/src/components/ViewAnime/ViewAnime.jsx
+++ client/src/components/ViewAnime/ViewAnime.jsx
@@ -6,13 +6,70 @@ import '../../scss/_ViewAnime.scss';
 
 const ViewAnime = ({ get_anime_id }) => {
 
-    const selected_anime = useSelector(state => state.reducer.AnimeListContainer.data);
+    const selected_anime = useSelector(state => get_anime_id ? state.reducer.AnimeListContainer.data : null);
 
     console.log(selected_anime);
 
   return (
     <div className='CONTAINER'>
+      <div className='anime'>
 
+        {/* container 1 */}
+        <div className='innerContainer-1'>
+          <div className='detailsContainer'>
+
+            {/* label here */}
+            <div className='titleContainer'>
+              <span className='text'>
+                {selected_anime?.title}
+              </span>
+            </div>
+
+          </div>
+          <div className='suggestionContainer'>
+
+            {/* label here */}
+            <div className='titleContainer'>
+              <span className='text'>
+                Popular animes
+              </span>
+            </div>
+
+          </div>
+        </div>
+
+        {/* container 2 */}
+        <div className='innerContainer-2'>
+
+          <div className='coverContainer'>
+
+            {/* label here */}
+            <div className='titleContainer'>
+              <span className='text'>
+                Cover
+              </span>
+            </div>
+
+            <div className='cover' style={{ backgroundImage: `url(${selected_anime?.images.jpg.large_image_url})` }}>
+
+            </div>
+
+          </div>
+
+          <div className='relatedAnimeContainer'>
+
+            {/* label here */}
+            <div className='titleContainer'>
+              <span span className='text'>
+                Related Anime
+              </span>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
     </div>
   )
 }
