af0f7fb8f3a05728b5d5ced126e756d55f488881
gino <gamexgaming1997@gmail.com>
2022-08-23 21:27:16 +0800

    done view anime

--- client/src/components/ViewAnime/ViewAnime.jsx
+++ client/src/components/ViewAnime/ViewAnime.jsx
@@ -9,13 +9,22 @@ import axios from 'axios';
 //scss
 import '../../scss/_ViewAnime.scss';
 
+// import action creator 
+import { get_mal_id_from_API } from '../../controller/thunk.js';
+
 const ViewAnime = ({ get_anime_id }) => {
+
+    //call action for action creator 
+    const dispatch = useDispatch();
 
     const navigate = useNavigate();
 
     const selected_anime = useSelector(state => get_anime_id ? state.reducer.AnimeListContainer?.data : null);
 
     const [recommended, setRecommended] = useState();
+    const [selectedTop, setSelectedTop] = useState();
+    const [topAnimes, setTopAnimes] = useState([]);
+    const [selectedRecommendedAnime, SetSelectedRecommendedAnime] = useState('');
 
     useEffect(()=> {
       const find = () => {
@@ -30,7 +39,34 @@ const ViewAnime = ({ get_anime_id }) => {
       if(selected_anime?.mal_id) find();
     },[selected_anime?.mal_id]);
 
-    // console.log(recommended);
+    useEffect(()=>{
+      const fetch = async () => {
+        axios({
+          method: 'get',
+          url: `https://api.jikan.moe/v4/top/anime?type=tv&filter=bypopularity&page=1&limit=25`,
+          withCredentials: false,
+          credentials: 'same-origin'
+        }).then(res => setTopAnimes(res.data.data))
+        .catch(err => console.log(err));
+      }
+      fetch();
+    },[])
+
+    const HandleSelected_Recommended = (e) => {
+      e.preventDefault();
+      if(selectedRecommendedAnime) {
+        dispatch(get_mal_id_from_API(selectedRecommendedAnime))
+        navigate(`/anime/view`)
+      }
+    }
+
+    const HandleSelected_TopAnime = (e) => {
+      e.preventDefault();
+      if(selectedTop){
+        dispatch(get_mal_id_from_API(selectedTop))
+        navigate('/anime/view')
+      }
+    }
 
   return (
     <div>
@@ -93,7 +129,22 @@ const ViewAnime = ({ get_anime_id }) => {
                   </div>
       
                   <div className='popularanimelistContainer'>
-
+                      {topAnimes && Object.keys(topAnimes).map(state => {
+                        return (
+                          <form onSubmit={HandleSelected_TopAnime} key={topAnimes[state].mal_id}>
+                          <button className='animesNames'
+                          onClick={()=>{
+                            setSelectedTop(topAnimes[state].mal_id)
+                          }}
+                          type='submit'
+                          >
+                              <span className='text'>
+                                {topAnimes[state].title}
+                              </span>
+                          </button>
+                          </form>
+                        )
+                      })}
                   </div>
       
                 </div>
@@ -126,8 +177,21 @@ const ViewAnime = ({ get_anime_id }) => {
 
                   {/* recommended animelist */}
                   <div className='suggestionslistContainer'>
-                    {recommended?.entry && Object.keys(recommended?.entry).map(val => {
-                      console.log(recommended?.entry[val].mal_id)
+                    {recommended && recommended.map(state =>{
+                      return (
+                        <form onSubmit={HandleSelected_Recommended} key={state?.entry.mal_id}>
+                          <button className='animeTitleBtn'
+                          onClick={()=> {
+                            SetSelectedRecommendedAnime(state?.entry.mal_id)
+                          }}
+                          type='submit'
+                          >
+                            <span className='text'>
+                              {state?.entry.title}
+                            </span>
+                          </button>
+                        </form>
+                      )
                     })}
                   </div>
       
